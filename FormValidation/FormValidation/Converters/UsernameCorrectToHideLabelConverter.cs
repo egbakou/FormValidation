7db69094555ef22e4f8ec51c1137652759b0f808
egbakou <egbakoukodjolaurent@gmail.com>
2019-03-23 06:22:40 +0000

    UsernameCorrect to bool Converter added

---
+++ FormValidation/FormValidation/Converters/UsernameCorrectToHideLabelConverter.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Globalization;
+using System.Text.RegularExpressions;
+using Xamarin.Forms;
+
+namespace FormValidation.Converters
+{
+    /// <summary>
+    /// Converter that check if username is correct and return true, otherwise return false.
+    /// The idea is to be able to show or hide username error label.
+    /// </summary>
+    public class UsernameCorrectToHideLabelConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return isUserNameCorrect(value);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        private bool isUserNameCorrect(object value)
+        {
+            if (value == null || ((string)value).Length == 0)
+                return true;
+
+            if (value is string)
+            {
+                bool isEmail = Regex.IsMatch(
+                    (string)value, "^[a-z0-9._-]+@[a-z0-9._-]+\\.[a-z]{2,6}$");
+
+                int length = ((string)value).Trim().Length;
+                if (length >= 7 && length <= 60 && isEmail)
+                    return true;
+                else
+                    return false;
+
+            }
+            return false;
+        }
+    }
+}
