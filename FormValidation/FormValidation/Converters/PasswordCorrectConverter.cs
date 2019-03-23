61d99b11ca58097b9d7fc90d688686ed898f2ddc
egbakou <egbakoukodjolaurent@gmail.com>
2019-03-23 06:21:12 +0000

    PasswordCorrect to bool Converter added

---
+++ FormValidation/FormValidation/Converters/PasswordCorrectConverter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace FormValidation.Converters
+{
+    /// <summary>
+    /// Converter that check if passsword is correct and return true, otherwise return false.
+    /// </summary>
+    public class PasswordCorrectConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return isPasswordCorrect(value);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        private bool isPasswordCorrect(object value)
+        {
+            if (value is string)
+            {
+                int length = ((string)value).Trim().Length;
+                if (length >= 8)
+                    return true;
+                else
+                    return false;
+            }
+            return false;
+        }
+    }
+}
---
+++ FormValidation/FormValidation/Converters/PasswordCorrectToHideLabelConverter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace FormValidation.Converters
+{
+    /// <summary>
+    /// Converter that check if password is correct and return true, otherwise return false.
+    /// The idea is to be able to show or hide password error label.
+    /// </summary>
+    class PasswordCorrectToHideLabelConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return isPasswordCorrect(value);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        private bool isPasswordCorrect(object value)
+        {
+            if (value == null || ((string)value).Length == 0)
+                return true;
+
+            if (value is string)
+            {
+                int length = ((string)value).Trim().Length;
+                if (length >= 8)
+                    return true;
+                else
+                    return false;
+            }
+            return false;
+        }
+    }
+}
