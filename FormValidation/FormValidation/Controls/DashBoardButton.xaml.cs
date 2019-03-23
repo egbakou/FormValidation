4c8e4d47cab27c3c273a806fc5382db155e57c48
egbakou <egbakoukodjolaurent@gmail.com>
2019-03-23 06:17:04 +0000

    Custom control added

---
+++ FormValidation/FormValidation/Controls/DashBoardButton.xaml.cs
@@ -0,0 +1,24 @@
+﻿using Xamarin.Forms;
+
+namespace FormValidation.Controls
+{
+    public partial class DashBoardButton : ContentView
+	{
+		public DashBoardButton ()
+		{
+			InitializeComponent ();
+		}
+
+        public ImageSource Icon
+        {
+            get { return DashBoardIcon.Source; }
+            set { DashBoardIcon.Source = value; }
+        }
+
+        public string Label
+        {
+            get { return DashBoardLabel.Text; }
+            set { DashBoardLabel.Text = value; }
+        }
+	}
+}
