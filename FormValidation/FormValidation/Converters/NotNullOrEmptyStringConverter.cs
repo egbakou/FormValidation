611b085e0752c41506a03a9f8d72cd76146bd820
egbakou <egbakoukodjolaurent@gmail.com>
2019-03-23 06:19:10 +0000

    NotNullOrEmptyString to bool Converter added

---
+++ FormValidation/FormValidation/Converters/NotNullOrEmptyStringConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace FormValidation.Converters
+{
+    /// <summary>
+    /// Converter that check if string is not null or Empty and retuen true or false.
+    /// </summary>
+    public class NotNullOrEmptyStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (value is string) ?  ((string) value).Length >0 : false;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
